b30a314fa3bed4f45b0f18655d4108baf3a80b13
Jaron-Lane <jaronwohlwend@icloud.com>
2024-11-04 18:00:47 -0600

    added sleep delays to see step progression

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+
 from selenium import webdriver
 
 # Open browser
 driver = webdriver.Chrome()
+time.sleep(3)
 
 # Go to webpage
 driver.get("https://practicetestautomation.com/practice-test-login/")
+time.sleep(5)
